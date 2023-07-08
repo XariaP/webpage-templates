85f9d2d81b20043798b6935baa93329f22962bb0
Xaria Prempeh <84147771+XariaP@users.noreply.github.com>
2023-07-07 23:23:19 -0400

    Update song selection & UI
    Added header to display current song selected.
    Added song description: artist, year, etc.
    Play button appears on hover.
    Added control panel.

--- music-player/script.js
+++ music-player/script.js
@@ -1,12 +1,17 @@
 var lastSelected = 0;
 
-function selectItem(num){
-    // console.log(num);
-    // alert(num);
-    document.getElementsByClassName("block-item")[2 + lastSelected].setAttribute("id", "h");
-    document.getElementsByClassName("block-item")[2 + num - 1].setAttribute("id", "selected");
-    lastSelected = num - 1;
-    document.getElementById("title").innerHTML = "Item " + num;
+function selectItem(albumi){
+    document.getElementsByClassName("block-item")[2 + lastSelected].setAttribute("id", "");
+    document.getElementsByClassName("block-item")[2 + albumi].setAttribute("id", "selected");
+    lastSelected = albumi;
+    
+    let num = parseInt(albumi) + 1
+    document.getElementById("title").innerHTML = "Album " + num;
+    let picIndex = parseInt(albumi) + 1;
+    document.getElementById("photo").setAttribute("src", "./images/pic" + picIndex + ".jpg");
+}
 
-    document.getElementById("photo").setAttribute("src", "./images/pic" + num + ".jpg");
+function selectTrack(albumi, tracki){
+    let num = parseInt(tracki) + 1
+    document.getElementById("track-name").innerText = "Track #" + num;
 }
